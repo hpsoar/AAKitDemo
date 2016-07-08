730037f7e03ee2555ad3b498da8084e5d8472300
Huang Peng <hpsoar@gmail.com>
2016-07-08 08:03:59 +0800

    Initial Commit

---
+++ AAKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AAKitDemo
+//
+//  Created by HuangPeng on 7/8/16.
+//  Copyright © 2016 Beacon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
